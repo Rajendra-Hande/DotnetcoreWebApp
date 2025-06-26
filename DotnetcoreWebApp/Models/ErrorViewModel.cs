66f77f19adf7cdc5351faac241075a0aca658097
Rajendra <rajendra.hande@annalect.com>
2025-06-26 21:04:42 +0530

    Add project files.

---
+++ DotnetcoreWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DotnetcoreWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
